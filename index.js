d7b731d378e7b9f34d5d1fc8abdbc9fb1f929888
chanakyasai <chanakya@infiniticube.in>
2024-12-08 18:30:20 +0530

    cors

--- index.js
+++ index.js
@@ -315,7 +315,7 @@ app.use("/peerjs", peerServer);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: ["http://localhost:3000", "http://localhost:5173","https://slack-clone-frontend-afsn.vercel.app",],
     methods: ["GET", "POST"],
   },
 });
@@ -354,7 +354,10 @@ peerServer.on('disconnect', (client) => {
 });
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:3000", "http://localhost:5173","https://slack-clone-frontend-afsn.vercel.app"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+}));
 app.use(express.json());
 app.use("/uploads", express.static("./uploads"));
 
